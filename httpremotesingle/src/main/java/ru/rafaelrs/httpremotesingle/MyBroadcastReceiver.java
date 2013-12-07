6c3976212906aa7a1a5fbb0ae47359b549828521
RafaelRS <rafaelrs@guitarplayer.ru>
2013-12-07 17:37:43 +0400

    Initial commit

---
+++ httpremotesingle/src/main/java/ru/rafaelrs/httpremotesingle/MyBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package ru.rafaelrs.httpremotesingle;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+/**
+ * Created by rafaelrs on 07.12.13.
+ */
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent startServiceIntent = new Intent(context, MainActivity.class);
+        context.startService(startServiceIntent);
+    }
+}
